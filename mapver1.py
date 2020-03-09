2ddd2f9caf7aa6ccf72a1c99639656b1ed58b2dc
lychank <60635703+lychank@users.noreply.github.com>
2020-03-09 11:14:05 +0800

    added files

---
+++ mapver1.py
@@ -0,0 +1,71 @@
+import os#os： This module provides a portable way of using operating system dependent functionality.
+#sys：This module provides access to some variables used or maintained by the interpreter and to functions that interact strongly with the interpreter.
+#os操作文件目录。sys针对系统环境的交互
+import pygame
+
+
+class Wall(object):#wall setting
+
+    def __init__(self, pos):
+        walls.append(self)
+        self.rect = pygame.Rect(pos[0], pos[1], 16, 16)
+
+
+# Initialise pygame
+os.environ["SDL_VIDEO_CENTERED"] = "1"#place the pygame screen in the middle
+pygame.init()
+
+# Set up the display
+pygame.display.set_caption("Get to the red square!")#the headline
+screen = pygame.display.set_mode((320, 240))#the size of the screen
+
+walls = []  # List to hold the walls
+
+
+# Holds the level layout in a list of strings.
+level = [
+    "WWWWWWWWWWWWWWWWWWWW",
+    "W                  W",
+    "W         WWWWWW   W",
+    "W   WWWW       W   W",
+    "W   W        WWWW  W",
+    "W WWW  WWWW        W",
+    "W   W     W W      W",
+    "W   W     W   WWW WW",
+    "W   WWW WWW   W W  W",
+    "W     W   W   W W  W",
+    "WWW   W   WWWWW W  W",
+    "W W      WW        W",
+    "W W   WWWW   WWW   W",
+    "W     W    E   W   W",
+    "WWWWWWWWWWWWWWWWWWWW",
+]
+
+# Parse the level string above. W = wall, E = exit
+x = y = 0
+for row in level:
+    for col in row:
+        if col == "W":
+            Wall((x, y))#thw wall block
+        if col == "E":
+            end_rect = pygame.Rect(x, y, 16, 16)#the exit block
+        x += 16
+    y += 16
+    x = 0
+
+running = True
+while running:
+
+    for e in pygame.event.get():#quit function
+        if e.type == pygame.QUIT:
+            running = False
+        if e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
+            running = False
+
+    # Draw the scene
+    screen.fill((0, 0, 0))#background color
+    for wall in walls:
+        pygame.draw.rect(screen, (255, 255, 255), wall.rect)#wall color
+    pygame.draw.rect(screen, (255, 0, 0), end_rect)#exit color
+    pygame.display.flip()#update the contents of the entire display
+    #pygame.display.update()#update a portion of the screen, instead of the entire area of the screen. Passing no arguments, updates the entire display
