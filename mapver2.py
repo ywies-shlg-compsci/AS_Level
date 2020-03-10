f5fe275c927b2c7fdd79609405c3ac0237d94144
lychank <60635703+lychank@users.noreply.github.com>
2020-03-10 09:39:43 +0800

    added

--- mapver2.py
+++ mapver2.py
@@ -1,7 +1,36 @@
 import os
 import pygame
 
+class Player(object):
 
+    def __init__(self):
+        self.rect = pygame.Rect(16, 208, 16, 16)
+
+    def move(self, dx, dy):
+
+        # Move each axis separately. Note that this checks for collisions both times.
+        if dx != 0:
+            self.move_single_axis(dx, 0)
+        if dy != 0:
+            self.move_single_axis(0, dy)
+
+    def move_single_axis(self, dx, dy):
+
+        # Move the rect
+        self.rect.x += dx
+        self.rect.y += dy
+
+        # If you collide with a wall, move out based on velocity
+        for wall in walls:
+            if self.rect.colliderect(wall.rect):
+                if dx > 0:  # Moving right; Hit the left side of the wall
+                    self.rect.right = wall.rect.left
+                if dx < 0:  # Moving left; Hit the right side of the wall
+                    self.rect.left = wall.rect.right
+                if dy > 0:  # Moving down; Hit the top side of the wall
+                    self.rect.bottom = wall.rect.top
+                if dy < 0:  # Moving up; Hit the bottom side of the wall
+                    self.rect.top = wall.rect.bottom
 class Wall(object):
     def __init__(self, pos):
         walls.append(self)
@@ -13,10 +42,11 @@ os.environ["SDL_VIDEO_CENTERED"] = "1"
 pygame.init()
 
 # Set up the display
-pygame.display.set_caption("Get to the red square!")#the headline
+pygame.display.set_caption("Get to the red square!" )#the headline
 screen = pygame.display.set_mode((640, 240))#the size of the screen
-
+clock = pygame.time.Clock()
 walls = []  # List to hold the walls
+player = Player()
 
 
 # Holds the level layout in a list of strings.
@@ -31,7 +61,7 @@ level = [
     "W   W     W W W WWWW   W  W WW W       W",
     "W   WWW WWW W W W  WWW W WWw W W     W W",
     "W     W   W   W        W     W WWWWWWW W",
-    "WWW   W   WW WW WWWWW WWWWW  W         W",
+    "WWW   W   WW WW WWWWW WWWWW  W W       W",
     "W W             W   W W   WWWWWW   WWWWW",
     "W W   WWWWWWWWWWW W WWW W   W          W",
     "W     WE          W     W              W",
@@ -58,11 +88,25 @@ while running:
             running = False
         if e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
             running = False
+    key = pygame.key.get_pressed()
+    if key[pygame.K_LEFT]:
+        player.move(-2, 0)
+    if key[pygame.K_RIGHT]:
+        player.move(2, 0)
+    if key[pygame.K_UP]:
+        player.move(0, -2)
+    if key[pygame.K_DOWN]:
+        player.move(0, 2)
 
+    # Just added this to make it slightly fun ;)
+    if player.rect.colliderect(end_rect):
+        print("You win!")
+        break
     # Draw the scene
     screen.fill((0, 0, 0))#background color
     for wall in walls:
         pygame.draw.rect(screen, (255, 255, 255), wall.rect)#wall color
     pygame.draw.rect(screen, (255, 0, 0), end_rect)#exit color
+    pygame.draw.rect(screen, (255, 200, 0), player.rect)
     pygame.display.flip()#update the contents of the entire display
     #pygame.display.update()#update a portion of the screen, instead of the entire area of the screen. Passing no arguments, updates the entire display
