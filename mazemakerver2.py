d2c50fffd79ffdd4123f3985c1daf6d7d3e17e39
lychank <60635703+lychank@users.noreply.github.com>
2020-04-14 10:29:07 +0800

    Create mazemakerver2.py

---
+++ mazemakerver2.py
@@ -0,0 +1,17 @@
+class room():
+    def __init__(self,row,col, rowSize, colSize, relRow, relCol, name):
+        self.visited = False
+        self.rowPos = row # this is the upper left hand corner of the room (which could be bigger than 1 cell)
+        self.colPos = col
+        self.rowSize=rowSize
+        self.colSize= colSize
+        self.cells = []
+        self.neighbours = []
+        self.name = name
+        #self.name = "r:" + str(self.rowPos) + " c: " + str(self.colPos) + name
+    def addCell(self, row, col):
+        self.cells.append((row, col))
+
+class roombig():
+    
+
